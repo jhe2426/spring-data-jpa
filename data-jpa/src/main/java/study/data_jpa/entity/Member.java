e69aee225b0384cb7e970eda0d9a40e3b157a97f
jhe2426 <jhe2426@naver.com>
2026-08-24 16:28:23 +0900

    스프링 데이터 JPA - 프로젝트 환경설정

---
+++ data-jpa/src/main/java/study/data_jpa/entity/Member.java
@@ -0,0 +1,27 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    protected Member(Long id) {
+        this.id = id;
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public void changeUsername(String uesrname) {
+        this.username = username;
+    }
+}
