3a9dba2ae56fc74f22a1878804b0e36c9b2f2047
jhe2426 <jhe2426@naver.com>
2026-08-25 17:50:32 +0900

    예제 도메인 모델과 동작확인

--- data-jpa/src/main/java/study/data_jpa/entity/Member.java
+++ data-jpa/src/main/java/study/data_jpa/entity/Member.java
@@ -1,27 +1,45 @@
 package study.data_jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member(Long id) {
-        this.id = id;
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
 
-    public void changeUsername(String uesrname) {
+    public Member(String username, int age, Team team) {
         this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeUsername(String username) {
+        this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
