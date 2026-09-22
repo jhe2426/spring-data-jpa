7254279669c91a6dbadb893fbfa813a7f4a86c52
jhe2426 <jhe2426@naver.com>
2026-09-22 18:46:53 +0900

    프로젝션과 결과 반환 - 기본

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -653,4 +653,40 @@ public class QuerydslBasicTest {
             System.out.println("s = " + s);
         }
     }
+
+    /*
+        프로젝션: select 대상을 의미
+        프로젝션 대상이 하나면 타입을 명확하게 지정할 수 있음
+        프로젝션 대상이 둘 이상이면 튜플이나 DTO로 조회
+    */
+    @Test
+    public void simpleProjection() {
+        // when
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        // then
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+    
+    @Test
+    public void tupleProjection() {
+        // when
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        // then
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
