ef92436e6491dcadb3b4502533d0ad24a160f856
jhe2426 <jhe2426@naver.com>
2026-09-24 23:52:45 +0900

    SQL function 호출하기

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -990,4 +990,37 @@ public class QuerydslBasicTest {
             System.out.println("member = " + member);
         }
     }
+
+    @Test
+    public void sqlFunction() {
+        // when
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        // then
+        for (String member : result) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    @Test
+    public void sqlFunction2() {
+        // when
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        // then
+        for (String member : result) {
+            System.out.println("member = " + member);
+        }
+    }
 }
