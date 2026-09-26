e6b9bb15206f5d986c79b96c28da80500d256bb9
jhe2426 <jhe2426@naver.com>
2026-09-26 19:23:56 +0900

    스프링 데이터 페이징 활용2 - CountQuery 최적화

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -5,6 +5,7 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
@@ -51,7 +52,14 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
         List<MemberTeamDto> content = getMemberTeamDtos(condition, pageable);
         JPAQuery<Long> countQuery = getCountQuery(condition);
 
+        /*
+            PageableExecutionUtils는 count 쿼리가 생략 가능한 경우 생략해서 처리해주는 스프링 데이터 라이브러리
+            count 쿼리 생략이 가능한 경우
+                - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+                - 마지막 페이지일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈를 구함)
+        */
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+//        return new PageImpl<>(content, pageable, countQuery.fetchOne());
     }
 
     private List<MemberTeamDto> getMemberTeamDtos(MemberSearchCondition condition, Pageable pageable) {
